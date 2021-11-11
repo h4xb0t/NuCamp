6a64d60aea173a427ea8e68aeee34fc3fc0ca3e8
Andrew Pratt <apratt@riotgames.com>
2021-11-11 02:42:42 +0000

    added more val code

--- Valorant/val_pkg/buy.py
+++ Valorant/val_pkg/buy.py
@@ -4,24 +4,60 @@ from time import sleep
 
 
 def player_wallet(wallet, round, round_result):
-    '''Player1 Wallet Tracker'''
-    if round == 13:
+    '''Player Wallet Tracker'''
+    if round == 12:
         wallet = 800
-        print(f"You have {wallet} credits")
+        print(f"\nYou have {wallet} credits")
         return wallet
     if round_result == "victory":
         wallet += 2000
-        print(f"You have {wallet} credits")
+        if wallet >= 16000:
+            wallet = 16000
+        print(f"\nYou have {wallet} credits")
         return wallet
     if round_result == "defeat":
         wallet += 1000
-        print(f"You have {wallet} credits")
+        if wallet >= 16000:
+            wallet = 16000
+        print(f"\nYou have {wallet} credits")
         return wallet
     if round == 1:
         wallet = 800
-        print(f"You have {wallet} credits")
+        print(f"\nYou have {wallet} credits")
         return wallet
     return wallet
+
+
+def agent_select():
+    '''Agent Select'''
+    while True:
+        print("1─►Jett")
+        print("2─►Phoenix")
+        print("3─►Sage")
+        print("4─►Brimstone")
+        print("5─►Sova")
+        agent_choice = input("\nSelect your agent: ").lower()
+        if agent_choice in ("1", "jett"):
+            agent = "Jett"
+            break
+        if agent_choice in ("2", "phoenix"):
+            agent = "Phoenix"
+            break
+        if agent_choice in ("3", "sage"):
+            agent = "Sage"
+            break
+        if agent_choice in ("4", "brimstone"):
+            agent = "Brimstone"
+            break
+        if agent_choice in ("5", "sova"):
+            agent = "Sova"
+            break
+        else:
+            print("\nPlease choose an agent\n")
+            continue
+    print(f"\n|| You have chosen {agent}. ||\n")
+    # sleep(1)
+    return agent
 
 
 def player_purchase(wallet):
@@ -32,83 +68,101 @@ def player_purchase(wallet):
         print("2─►Judge ---- 1800")
         print("3─►Phantom -- 2900")
         print("4─►Operator - 5000")
-        # print(f"You have {wallet} credits")
-        weapon = input("\nChoose your weapon: ").lower()
+        # print("5─►Keep Current --")
+        weapon = input("\nSelect your weapon: ").lower()
         if weapon in ('1', 'ghost'):
             if wallet >= 500:
                 wallet -= 500
                 weapon = "ghost"
-                print(f"|| Purchased the {weapon}. ||".title())
+                player_purchase.weapon = "ghost"
+                print(f"\n|| Purchased the {weapon}. ||".title())
                 sleep(1)
-                print(f"{wallet} credits remaining.")
+                print(f"\nYou have {wallet} credits remaining.")
                 sleep(1)
                 return wallet
             else:
-                print("You don't have enough to purchase the Ghost!")
+                print("\nYou don't have enough to purchase the Ghost!")
                 continue
         if weapon in ('2', 'judge'):
             if wallet >= 1800:
                 wallet -= 1800
                 weapon = "judge"
-                print(f"|| Purchased the {weapon}. ||".title())
+                player_purchase.weapon = "judge"
+                print(f"\n|| Purchased the {weapon}. ||".title())
                 sleep(1)
-                print(f"{wallet} credits remaining.")
+                print(f"\nYou have {wallet} credits remaining.")
                 sleep(1)
                 return wallet
             else:
-                print("You don't have enough to purchase the Judge!")
+                print("\nYou don't have enough to purchase the Judge!")
                 continue
         if weapon in ('3', 'phantom'):
             if wallet >= 2900:
                 wallet -= 2900
                 weapon = "phantom"
-                print(f"|| Purchased the {weapon}. ||".title())
+                player_purchase.weapon = "phantom"
+                print(f"\n|| Purchased the {weapon}. ||".title())
                 sleep(1)
-                print(f"{wallet} credits remaining.")
+                print(f"\nYou have {wallet} credits remaining.")
                 sleep(1)
                 return wallet
             else:
-                print("You don't have enough to purchase the Phantom!")
+                print("\nYou don't have enough to purchase the Phantom!")
                 continue
         if weapon in ('4', 'operator'):
             if wallet >= 5000:
                 wallet -= 5000
                 weapon = "operator"
-                print(f"|| Purchased the {weapon}. ||".title())
+                player_purchase.weapon = "operator"
+                print(f"\n|| Purchased the {weapon}. ||".title())
                 sleep(1)
-                print(f"{wallet} credits remaining.")
+                print(f"\nYou have {wallet} credits remaining.")
                 sleep(1)
                 return wallet
             else:
-                print("You don't have enough to purchase the Operator!")
+                print("\nYou don't have enough to purchase the Operator!")
                 continue
 
 
-def cpu_wallet_track(cpu_wallet, round, last_round_result):
-    '''CPU Wallet Tracker'''
-    # wallet = 0
+def bot_wallet_func(bot_wallet, round, round_result):
+    '''bot Wallet Tracker'''
     if round == 13:
-        cpu_wallet = 800
-        return cpu_wallet
-    if last_round_result == "victory":
-        cpu_wallet += 2000
-        return cpu_wallet
-    if last_round_result == "defeat":
-        cpu_wallet += 800
-        return cpu_wallet
+        bot_wallet = 800
+        return bot_wallet
+    if round_result == "victory":
+        bot_wallet += 2000
+        if bot_wallet >= 16000:
+            bot_wallet = 16000
+        return bot_wallet
+    if round_result == "defeat":
+        bot_wallet += 1000
+        if bot_wallet >= 16000:
+            bot_wallet = 16000
+        return bot_wallet
     if round == 1:
-        cpu_wallet = 800
-        return cpu_wallet
-    return cpu_wallet
+        bot_wallet = 800
+        return bot_wallet
+    return bot_wallet
 
 
-def cpu_purchase(cpu_wallet):
-    '''CPU Buy Process'''
-    if cpu_wallet >= 5000:
-        cpu_weapon = "operator"
-    if cpu_wallet >= 2900:
-        cpu_weapon = "phantom"
-    if cpu_wallet >= 1800:
-        cpu_weapon = "judge"
-    elif cpu_wallet >= 500:
-        cpu_weapon = "ghost"
+def bot_purchase(bot_wallet):
+    '''bot Buy Process'''
+    if bot_wallet >= 5000:
+        bot_wallet -= 5000
+        bot_purchase.bot_weapon = "operator"
+        return bot_wallet
+    if bot_wallet >= 2900:
+        bot_wallet -= 2900
+        bot_purchase.bot_weapon = "phantom"
+        return bot_wallet
+    if bot_wallet >= 1800:
+        bot_wallet -= 1800
+        bot_purchase.bot_weapon = "judge"
+        return bot_wallet
+    elif bot_wallet >= 500:
+        bot_wallet -= 500
+        bot_purchase.bot_weapon = "ghost"
+        return bot_wallet
+    return bot_wallet
+
+# maybe can check weapon existing i.e. if weapon == ghost; then weapon == ghost
